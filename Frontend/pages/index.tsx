24bc2433c64c5ac92a1d79a2747870f13c6f716d
POLLY1515 <amarantepolly2030@gmail.com>
2026-04-23 09:37:05 -0300

    Initial commit

---
+++ Frontend/pages/index.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+
+const Home: React.FC = () => {
+
+  return (
+  <div>
+    <head>
+      <title>vendas app</title>
+    </head>
+
+    bem vindo
+  </div>
+
+)
+}
+
+export default  Home;
+
+

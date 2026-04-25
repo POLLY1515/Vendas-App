1438814106f4bdf2c5585e3f511a429151b1d4bf
POLLY1515 <amarantepolly2030@gmail.com>
2026-04-24 21:54:17 -0300

    Implementando menu lateral

--- Frontend/src/components/Layout/index.tsx
+++ Frontend/src/components/Layout/index.tsx
@@ -1,8 +1,10 @@
+import {Menu} from './Menu'
+
 export const Layout: React.FC = () =>{
     return(
         <div className="app">
             <section className="main-content columns is-fullheight">
-                {/**menu ficara aqui */}
+                <Menu />
                 <div className="container column is-10">
                     <div className="section">
                         <div className="card">

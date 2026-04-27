7fddfa3bb1d4f2215ecfa9d8a971f64cddd00ee7
POLLY1515 <amarantepolly2030@gmail.com>
2026-04-27 10:01:39 -0300

    Cadastro de produtos

--- Frontend/src/components/Layout/index.tsx
+++ Frontend/src/components/Layout/index.tsx
@@ -1,6 +1,47 @@
 import {Menu} from './Menu'
 
-export const Layout: React.FC = () =>{
+interface LayoutProps{
+    titulo?: string;
+    children?: Reacimport Link from 'next/link'
+import { signOut } from 'next-auth/client'
+
+export const Menu: React.FC = () => {
+    return (
+        <aside className="column is-2 is-narrow-mobile is-fullheight section is-hidden-mobile">
+            <p className="menu-label is-hidden-touch">
+                Minhas Vendas
+            </p>
+            <ul className="menu-list">
+                <MenuItem href="/" label="Home"  />
+                <MenuItem href="/consultas/produtos" label="Produtos"  />
+                <MenuItem href="/consultas/clientes" label="Clientes" />
+                <MenuItem href="/vendas/nova-venda" label="Venda"  />
+                <MenuItem href="/vendas/relatorio-vendas" label="Relatório"  />
+                <MenuItem onClick={() => signOut()} href="/" label="Sair"  />
+            </ul>
+        </aside>
+    )
+}
+
+interface MenuItemProps {
+    href: string;
+    label: string;
+    onClick?: () => void;
+}
+
+const MenuItem: React.FC<MenuItemProps> = (props: MenuItemProps) => {
+    return (
+        <li>
+            <Link href={props.href}>
+                <a onClick={props.onClick}>
+                    <span className="icon"></span> { props.label } 
+                </a>
+            </Link>
+        </li>
+    )
+}tNode;
+}
+export const Layout: React.FC<LayoutProps> = (props:LayoutProps) =>{
     return(
         <div className="app">
             <section className="main-content columns is-fullheight">
@@ -10,12 +51,12 @@ export const Layout: React.FC = () =>{
                         <div className="card">
                             <div className="card-header">
                                 <p className="card-header-title">
-                                    cadastro
+                                    {props.titulo}
                                 </p>
                             </div>
                             <div className="card-content">
                                     <div className="content">
-                                        conteudo
+                                        {props.children}
                                     </div>
                             </div>
                         </div>
---
+++ Frontend/src/components/produtos/cadastro/index.tsx
@@ -0,0 +1,9 @@
+import {Layout} from 'components'
+
+export const CadastroProdutos: React.FC = () =>{
+    return(
+        <Layout titulo="Cadastro de produtos">
+
+        </Layout>
+    )
+}

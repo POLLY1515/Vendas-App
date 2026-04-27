cd75e7082e0ecd7e40635b298120c795c6ee5a56
POLLY1515 <amarantepolly2030@gmail.com>
2026-04-27 08:28:27 -0300

    Componentizando os itens de menu

--- Frontend/src/components/Layout/Menu/index.tsx
+++ Frontend/src/components/Layout/Menu/index.tsx
@@ -1,30 +1,35 @@
+
+import Link from 'next/link'
+
 export const Menu: React.FC = () => {
 return(
     <aside className="column is-2 is-narrow-mobile is-fullheight section is-hidden-mobile">
-        <p >Minhas vendas</p>
+        <p>
+            Minhas vendas
+            </p>
         <ul className="menu-list">
-            <li>
-                <a href="#">
-                    <span className="icon"></span> Home
-                </a>
-            </li>
+           <MenuItem  href='/' label='Home'/>
+            <MenuItem  href='/' label='Cadastros'/>
+             <MenuItem  href='/' label='Config'/>
         </ul>
-        <ul className="menu-list">
-            <li>
-                <a href="#">
-                    <span className="icon"></span> Cadastros
-                </a>
-            </li>
-        </ul>
-        <ul className="menu-list">
-            <li>
-                <a href="#">
-                    <span className="icon"></span> Config
-                </a>
-            </li>
-        </ul>
-        
     </aside>
-)
+    )
 
+}
+
+interface MenuItemProps{
+    href: string; /*Pra onde vai*/ 
+    label:string; /*Texo que vai aparecer*/ 
+}
+
+const MenuItem: React.FC<MenuItemProps> = (props: MenuItemProps) => {
+    return(
+        <li>   
+             <Link href={props.href}>
+                <a>
+                    <span className="icon"></span> {props.label}
+                </a>
+                </Link>
+        </li>
+    )
 }
